e2fcea0678ca88a84cb7af8bb070315b868bcdc2
Just van den Broecke <just@justobjects.nl>
2020-04-28 18:29:54 +0200

    #34 save work for testing master

--- app/components/layerlist/LayerList.vue
+++ app/components/layerlist/LayerList.vue
@@ -1,42 +1,85 @@
+<i18n>
+de:
+  POIs: POIs
+  Routes: Strecke
+  Areas: Gebiete
+  Categories: Kategorien
+  Tags: Tags
+</i18n>
 <template>
-
-  <v-list>
-    <v-treeview
-            :items="items"
-            :load-children="fetchItems"
-            :open.sync="open"
-          >
+  <div>
+    <v-tabs
+      dark
+      slider-color="grey"
+    >
+      <v-tab>
+        {{ $t(categoriesTitle) }}
+      </v-tab>
+      <v-tab-item>
+        <v-list>
+          <v-treeview :items="categoriesTree" :load-children="fetchCategoryItems" :open.sync="open">
             <template v-slot:prepend="{ item }">
-              <input type="checkbox" :key="item.lid" class="wgu-layer-viz-cb" v-model="item.visible" @change="layerVizChanged($event, item)">
-<!--              <v-list-tile-content class="black&#45;&#45;text">-->
-<!--                  <v-list-tile-title>{{ item.name }}</v-list-tile-title>-->
-<!--              </v-list-tile-content>-->
-              <v-list-tile-avatar>
-                <img v-if="item.category === 'poi'" v-bind:src="item.icon" alt="POI Icon">
-                <v-card v-if="item.category === 'route'"
-                                  :style="{
-                                    background: item.lineColor,
-                                  }"
-                                  width="30"
-                                  height="4"
-                                  class="mr-2"
-                                />
-                <v-card v-if="item.category === 'area'"
-                                  :style="{
-                                    background: item.fillColor,
-                                    border: '3px solid',
-                                    borderColor: item.lineColor
-                                  }"
-                                  width="30"
-                                  height="30"
-                                  class="mr-2"
-                                />
-              </v-list-tile-avatar>
+              <input type="checkbox" :key="item.lid" class="wgu-layer-viz-cb" v-model="item.visible" @change="layerVizChanged(item)">
+              <img v-if="item.category === 'poi'" v-bind:src="item.icon" alt="POI Icon">
+              <v-card v-if="item.category === 'route'"
+                                :style="{
+                                  background: item.lineColor,
+                                }"
+                                width="30"
+                                height="4"
+                                class="mr-2"
+                              />
+              <v-card v-if="item.category === 'area'"
+                                :style="{
+                                  background: item.fillColor,
+                                  border: '3px solid',
+                                  borderColor: item.lineColor
+                                }"
+                                width="30"
+                                height="30"
+                                class="mr-2"
+                              />
             </template>
           </v-treeview>
-<!--       <v-list-tile class="wgu-layerlist-item" v-for="layerItem in layerItems" v-bind:key="item.lid" @click="onItemClick($event, layerItem)">-->
-<!--    </v-list-tile>-->
-  </v-list>
+        </v-list>
+      </v-tab-item>
+      <v-tab>
+        {{ $t(tagsTitle) }}
+      </v-tab>
+      <v-tab-item>
+        <v-list>
+           <v-treeview
+                   :items="tagsTree"
+                   :load-children="fetchTagItems"
+                   :open.sync="open"
+                 >
+                   <template v-slot:prepend="{ item }">
+                     <input type="checkbox" :key="item.lid" class="wgu-layer-viz-cb" v-model="item.visible" @change="layerVizChanged(item)">
+                     <img v-if="item.category === 'poi'" v-bind:src="item.icon" alt="POI Icon">
+                     <v-card v-if="item.category === 'route'"
+                                       :style="{
+                                         background: item.lineColor,
+                                       }"
+                                       width="30"
+                                       height="4"
+                                       class="mr-2"
+                                     />
+                     <v-card v-if="item.category === 'area'"
+                                       :style="{
+                                         background: item.fillColor,
+                                         border: '3px solid',
+                                         borderColor: item.lineColor
+                                       }"
+                                       width="30"
+                                       height="30"
+                                       class="mr-2"
+                                     />
+                   </template>
+                 </v-treeview>
+         </v-list>
+      </v-tab-item>
+    </v-tabs>
+  </div>
 
 </template>
 
@@ -48,18 +91,24 @@
     name: 'wgu-layerlist',
     mixins: [Mapable],
     props: {
+      categoriesTitle: {type: String, required: false, default: 'Categories'},
+      tagsTitle: {type: String, required: false, default: 'Tags'}
     },
     data () {
       return {
         layerItems: [],
         categoryItems: [],
+        tagItems: [],
         open: [1],
         changeVisByClickUpdate: false
       }
     },
     computed: {
-      items () {
+      categoriesTree () {
         return this.categoryItems
+      },
+      tagsTree () {
+        return this.tagItems
       }
     },
     methods: {
@@ -75,13 +124,46 @@
         });
       },
       /**
+       * Creates single layer item from an OpenLayers Layer.
+       */
+      createLayerItem (layer) {
+        const layerId = layer.get('lid');
+        const layerCategory = layerId.split('_')[0];
+        let layerStyle = layer.getStyle();
+        let icon, lineColor, fillColor;
+        if (layerCategory === 'route') {
+          lineColor = layerStyle.getStroke().getColor();
+        } else if (layerCategory === 'area') {
+          lineColor = layerStyle.getStroke().getColor();
+          fillColor = layerStyle.getFill().getColor();
+        } else if (layerCategory === 'poi') {
+          if (Array.isArray(layerStyle)) {
+            layerStyle = layerStyle[1];
+          }
+          icon = layerStyle.getImage().getSrc();
+        }
+
+        return {
+          id: -1,
+          name: layer.get('name'),
+          lid: layerId,
+          category: layerCategory,
+          tags: layer.get('tags'),
+          icon: icon,
+          lineColor: lineColor,
+          fillColor: fillColor,
+          visible: layer.getVisible()
+        };
+      },
+      /**
        * Creates the layer items from the OpenLayers map.
        */
       createLayerItems () {
         // go over all (reversed) layers from the map and list them up
         const layers = this.map.getLayers().getArray().slice(0).reverse();
 
-        let items = [
+        // Predefined Categories
+        let categoryItems = [
           {
             id: 1,
             name: 'POIs',
@@ -90,70 +172,91 @@
             ]
           },
           {
-            id: 15,
+            id: 20,
             name: 'Routes',
             lid: undefined,
             children: [
             ]
           },
           {
-            id: 19,
+            id: 30,
             name: 'Areas',
             lid: undefined,
             children: [
             ]
           }];
 
-        let categoryItems = items[0]['children'];
-        layers.forEach((layer) => {
+        // Tag defs come from the tags added to Layers
+        let tagItems = [];
+        let nextId = 4;
+        layers.forEach((layer, idx) => {
           // skip if layer should not be listed
           if (layer.get('displayInLayerList') === false) {
             return;
           }
-          const layerId = layer.get('lid');
-          const layerCategory = layerId.split('_')[0];
-          let layerStyle = layer.getStyle();
-          let icon, lineColor, fillColor;
+          let layerItem = this.createLayerItem(layer);
+          layerItem.id = nextId;
+          nextId += 1;
+          // First add to Categories tree
+          const layerCategory = layerItem.category;
+          let categoryNode = categoryItems[0]['children'];
           if (layerCategory === 'route') {
-            lineColor = layerStyle.getStroke().getColor();
-            categoryItems = items[1]['children'];
+            categoryNode = categoryItems[1]['children'];
           } else if (layerCategory === 'area') {
-            lineColor = layerStyle.getStroke().getColor();
-            fillColor = layerStyle.getFill().getColor();
-            categoryItems = items[2]['children'];
-          } else if (layerCategory === 'poi') {
-            if (Array.isArray(layerStyle)) {
-              layerStyle = layerStyle[1];
-            }
-            icon = layerStyle.getImage().getSrc();
-            categoryItems = items[0]['children'];
+            categoryNode = categoryItems[2]['children'];
+          }
+          categoryNode.push(layerItem);
+
+          // Skip if no tags
+          if (!layerItem.tags) {
+            return;
           }
 
-          const layerItem = {
-            id: categoryItems.length + 100,
-            name: layer.get('name'),
-            lid: layerId,
-            category: layerCategory,
-            icon: icon,
-            lineColor: lineColor,
-            fillColor: fillColor,
-            visible: layer.getVisible()
-          };
+          // One or more Tags avail: add to the Tags tree
+          layerItem = this.createLayerItem(layer);
+          layerItem.id = nextId;
+          nextId += 1;
+          const tags = layerItem.tags;
+          tags.forEach((tag, idx) => {
+            let tagNode;
+            tagItems.forEach((tagItem, idx) => {
+              if (tagItem.name === 'tag') {
+                tagNode = tagItem;
+              }
+            });
 
-          categoryItems.push(layerItem);
+            if (!tagNode) {
+              tagNode = {
+                id: nextId,
+                name: tag,
+                lid: undefined,
+                children: [
+                ]
+              };
+              tagItems.push(tagNode)
+              nextId += 1;
+            }
+            tagNode = tagNode.children.push(layerItem)
+          });
+
           this.layerItems.push(layerItem);
 
           // synchronize visibility with UI when changed programatically
-          layer.on('change:visible', (evt) => {
-            this.onOlLayerVizChange(evt)
-          });
+          // layer.on('change:visible', (evt) => {
+          //   this.onOlLayerVizChange(evt)
+          // });
         });
 
-        this.categoryItems = items;
+        this.categoryItems = categoryItems;
+        this.tagItems = tagItems;
       },
 
-      fetchItems () {
+      fetchCategoryItems () {
         return this.categoryItems;
+      },
+
+      fetchTagItems () {
+        return this.tagItems;
       },
 
       /**
@@ -193,11 +296,17 @@
        * Handles the 'change' event of the visibility checkboxes in order to
        * apply the current visibility state in 'data' to the OL layers.
        */
-      layerVizChanged (evt, item) {
-        this.layerItems.forEach((layerItem, idx) => {
-          const layer = LayerUtil.getLayerByLid(layerItem.lid, this.map);
+      layerVizChanged (item) {
+        if (item && item.children) {
+          item.children.forEach((item, idx) => {
+            this.onItemClick(null, item)
+          });
+          return;
+        }
+        this.layerItems.forEach((item, idx) => {
+          const layer = LayerUtil.getLayerByLid(item.lid, this.map);
           if (layer) {
-            layer.setVisible(layerItem.visible);
+            layer.setVisible(item.visible);
           }
         });
       }
@@ -208,7 +317,7 @@
 <style>
 
   .wgu-layer-viz-cb {
-    width: 45px;
+    width: 24px;
   }
 
 </style>
