dbf536f4a6b3c823827d8c63d796b42c33df0d3d
Just van den Broecke <just@justobjects.nl>
2020-05-18 17:45:38 +0200

    #59 now font loading works for prod builds but fails in dev mode...sigh

--- build/utils.js
+++ build/utils.js
@@ -16,6 +16,7 @@ exports.cssLoaders = function (options) {
     loader: 'css-loader',
     options: {
       minimize: process.env.NODE_ENV === 'production',
+      url: false,
       sourceMap: options.sourceMap
     }
   }
