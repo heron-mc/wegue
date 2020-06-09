989718c06222cc7b36beac16146dcfc65803b36c
Just van den Broecke <just@justobjects.nl>
2020-06-09 15:27:43 +0200

    First Heron v2 with Wegue demo

---
+++ app/WguApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <wgu-app-tpl>
+    <!-- insert your app slots here  -->
+  </wgu-app-tpl>
+</template>
+
+<script>
+  import WguAppTemplate from './WguAppTemplate.vue';
+  export default {
+    name: 'my-wgu-app',
+    components: {
+      'wgu-app-tpl': WguAppTemplate
+    }
+    // add Vue methods and hooks here
+  }
+</script>
